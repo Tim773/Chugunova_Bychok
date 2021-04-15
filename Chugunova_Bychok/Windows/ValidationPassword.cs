6b5c5a64210dc45cd69dc9a3b48b0800004a2e71
Tim773 <btimon@list.ru>
2021-04-15 21:09:16 +0300

    xd

--- Chugunova_Bychok/Windows/ValidationPassword.cs
+++ Chugunova_Bychok/Windows/ValidationPassword.cs
@@ -13,14 +13,36 @@ namespace Chugunova_Bychok.Windows
             
             if (a.Length >= 8 && a.Length <= 20)
             {
+                for (int i = 0; i < a.Length; i++)
+                    if ((a[i] >= '0') && (a[i] <= '9'))
+                    {
+                        for (int d = 0; d < a.Length; d++)
+                        
+                            if ((a[d] >= '#') && (a[d] <= '&') || a[d] == '^' || a[d] == '_')
+                            {
+                                for (int j = 0; j < a.Length; j++)
+                                    if ((a[j] >= 'a') && (a[j] <= 'z'))
+                                    {
+                                        for (int k = 0; k < a.Length; k++)
+                                        {
+                                            if ((a[k] >= 'A') && (a[k] <= 'Z'))
+                                                return true;
+                                        }
+                                        return false;
+                                    }
+                                    return false;
+                            }
+                            return false;
+                        
+                    }
+                    return false;
 
-
-                //for (int i = 0; i <a.Length; i++ )
+                //for (int i = 0; i < a.Length; i++)
                 //{
-                //    if (a[i] >= '' && a[i] <= 57)
+                //    if (a[i] >= '1' && a[i] <= '9')
                 //    {
 
-                       
+
                 //        return true;
 
 
@@ -34,37 +56,37 @@ namespace Chugunova_Bychok.Windows
                 //    if (ch >= 48 && ch <= 57)
                 //    {
 
-                //        //foreach (char ch1 in a)
-                //        //{
-                //        //    if (ch1 >= 35 && ch1 <= 38 || ch1 == 94 || ch1 == 95)
-                //        //    {
-                //        //        foreach (char ch2 in a)
-                //        //        {
-                //        //            if (ch2 >= 65 && ch2 <= 90)
-                //        //            {
-                //        //                foreach (char ch3 in a)
-                //        //                {
-                //        //                    if (ch3 >= 97 && ch3 <= 122)
-                //        //                    {
-                //        //                        return true;
-                //        //                    }
-                //        //                    else return false;
-                //        //                }
-                //        //                return true;
-                //        //            }
-                //        //            else return false;
-                //        //        }
-                //        //        return true;
-                //        //    }
-                //        //    else return false;
-                //        //}
+                //        foreach (char ch1 in a)
+                //        {
+                //            if (ch1 >= 35 && ch1 <= 38 || ch1 == 94 || ch1 == 95)
+                //            {
+                //                foreach (char ch2 in a)
+                //                {
+                //                    if (ch2 >= 65 && ch2 <= 90)
+                //                    {
+                //                        foreach (char ch3 in a)
+                //                        {
+                //                            if (ch3 >= 97 && ch3 <= 122)
+                //                            {
+                //                                return true;
+                //                            }
+                //                            else return false;
+                //                        }
+                //                        return true;
+                //                    }
+                //                    else return false;
+                //                }
+                //                return true;
+                //            }
+                //            else return false;
+                //        }
                 //        return true;
                 //    }
                 //    else return false;
                 //}
-                return true;
+                //return false;
             }
-            else return false;
+             return false;
             
         }
     }
